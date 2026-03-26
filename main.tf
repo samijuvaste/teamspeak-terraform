beb0712643e46e7224064bf2d207d5ee95577a8b
Sami Juvaste <sami@juvaste.fi>
2026-03-26 03:40:19 +0100

    Initail implementation

---
+++ main.tf
@@ -0,0 +1,115 @@
+resource "upcloud_server" "teamspeak" {
+  hostname = "teamspeak-server"
+  zone     = var.server_zone
+  plan     = var.server_plan
+  metadata = true
+  firewall = true
+
+  template {
+    storage = "Ubuntu Server 24.04 LTS (Noble Numbat)"
+    size    = var.server_size
+    encrypt = true
+  }
+
+  network_interface {
+    type = "public"
+  }
+
+  network_interface {
+    type = "utility"
+  }
+
+  login {
+    user            = "teamspeak"
+    keys            = [var.ssh_public_key]
+    create_password = false
+  }
+
+  # Render the cloud-init file, passing in the rendered docker-compose file
+  user_data = templatefile("${path.module}/cloud-init.yaml", {
+    docker_compose_content = templatefile("${path.module}/docker-compose.yaml", {
+      QUERY_ADMIN_PASSWORD = var.query_admin_password
+      MYSQL_ROOT_PASSWORD  = var.mysql_root_password
+      MYSQL_PASSWORD       = var.mysql_password
+      TIMEZONE             = var.timezone
+    })
+  })
+
+  storage_devices {
+    address          = "virtio"
+    address_position = 1
+    storage          = upcloud_storage.teamspeak_data.id
+  }
+
+  simple_backup {
+    plan = "daily"
+    time = "0200"
+  }
+}
+
+resource "upcloud_storage" "teamspeak_data" {
+  size    = var.teamspeak_data_size
+  tier    = var.teamspeak_data_tier
+  title   = "teamspeak-data"
+  zone    = var.server_zone
+  encrypt = true
+}
+
+# --- Firewall Rules ---
+resource "upcloud_firewall_rules" "ts_fw" {
+  server_id = upcloud_server.teamspeak.id
+
+  firewall_rule {
+    action                 = "accept"
+    direction              = "in"
+    family                 = "IPv4"
+    protocol               = "tcp"
+    destination_port_start = "22"
+    destination_port_end   = "22"
+  }
+
+  firewall_rule {
+    action                 = "accept"
+    direction              = "in"
+    family                 = "IPv4"
+    protocol               = "udp"
+    destination_port_start = "9987"
+    destination_port_end   = "9987"
+  }
+
+  firewall_rule {
+    action                 = "accept"
+    direction              = "in"
+    family                 = "IPv4"
+    protocol               = "tcp"
+    destination_port_start = "30033"
+    destination_port_end   = "30033"
+  }
+
+  firewall_rule {
+    action                 = "accept"
+    direction              = "in"
+    family                 = "IPv4"
+    protocol               = "tcp"
+    destination_port_start = "10022"
+    destination_port_end   = "10022"
+  }
+
+  firewall_rule {
+    action    = "drop"
+    direction = "in"
+  }
+}
+
+# --- Cloudflare DNS ---
+resource "cloudflare_record" "ts_dns" {
+  zone_id = var.cloudflare_zone_id
+  name    = var.subdomain
+  # The first network interface [0] is the public interface
+  content = upcloud_server.teamspeak.network_interface[0].ip_address
+  type    = "A"
+
+  # CRITICAL: Proxy MUST be false because Cloudflare's free tier proxy doesn't support UDP. 
+  # If this is true, nobody will be able to connect to the server.
+  proxied = false
+}
